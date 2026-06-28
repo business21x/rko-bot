fd0e670c457e7063b77a1274d7cfcadc8ad8ac9a
business21x <business_21x@bk.ru>
2026-06-28 17:13:33 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// ============================================
+// СЕКРЕТНЫЕ НАСТРОЙКИ (НЕ ДЕЛАЙТЕ ИХ ПУБЛИЧНЫМИ!)
+// Этот файл загружается отдельно от index.html
+// ============================================
+
+// Токен вашего Telegram бота
+const TELEGRAM_TOKEN = "8669530100:AAErkDlHyLIHdK9BKPW6soH8krvE5UHE_jQ";
+
+// ID вашего чата в Telegram
+const TELEGRAM_CHAT_ID = "756753408";
+
+// Ссылка на Google Apps Script (ваша таблица)
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxKFWYI4X9oAVZDg5aGzk7uQPk8ExxA2XpIx3wVuF-QaFZ4N9h_jy9PHde5W7mWlzMehA/exec";
